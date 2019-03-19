ebfb487fd7eb08160d360ab7f8c8e7b7bfd977ac
rhardis <38573978+rhardis@users.noreply.github.com>
2019-03-18 22:46:13 -0400

    correct dates
    correct dates after 10 trading days.

--- march-strategy/back_test.py
+++ march-strategy/back_test.py
@@ -15,20 +15,20 @@ import run_strat as rs
 def calculate_return(df, buy_date, period):
     df = df.copy(deep=True)
     df.reset_index(inplace=True)
-    sell_date = buy_date + timedelta(days=period)
+    df = rs.constrain_data(df, buy_date, end_date)
+    start_df = df[df.DT == buy_date]
+    start_list = start_df['Close']
+    start_price = start_list.iloc[0]
+    start_date_index = df[df.DT == buy_date].index.values.astype(int)[0]
+    end_price = df['Close'][start_date_index + period]
+    sell_date = df.DT[start_date_index + period]
     if (sell_date - df.DT.iloc[-1]).days > 0:
         print('end date out of range')
         net_return = np.nan
     else:
-        df = rs.constrain_data(df, buy_date, end_date)
-        start_df = df[df.DT == buy_date]
-        start_list = start_df['Close']
-        start_price = start_list.iloc[0]
-        start_date_index = df[df.DT == buy_date].index.values.astype(int)[0]
-        end_price = df['Close'][start_date_index + period]
         net_return = (end_price - start_price) / start_price * 100
     
-    return [sell_date, net_return]
+    return [start_price, end_price, sell_date, net_return]
 
 start_time = datetime.now()
 
@@ -43,9 +43,9 @@ days_until_sale = 10
 sd = rs.securityData()
 greater_df_list = []
 temp_list = ['SPY','MCK','AAPL']
-for ticker in sd.tickers[:500]: # this is normally ticker in sd.tickers to get all tickers on NYSE
+for ticker in ['SPY']:#sd.tickers[:500]: # this is normally ticker in sd.tickers to get all tickers on NYSE
     print(ticker)
-    unconstrained_data = rs.get_ticker_data(ticker, 'daily', '0')
+    unconstrained_data = rs.get_ticker_data(ticker.upper(), 'daily', '0')
     dates_df = unconstrained_data[(unconstrained_data.DT >= start_date) & (unconstrained_data.DT <= end_date)]
     dates_list = list(dates_df.DT)
     for day in dates_list:
@@ -61,13 +61,14 @@ for ticker in sd.tickers[:500]: # this is normally ticker in sd.tickers to get a
             df_list = [ticker]
             df_list.append(day)
             df_list.append(stoch_val)
-            df_list.append('Sell_Signal')
-            df_list.append(np.nan)
+            #df_list.append('Sell_Signal')
+            df_list.extend(calculate_return(unconstrained_data, day, days_until_sale))
+            #df_list.append(np.nan)
             greater_df_list.append(df_list)       
         
             
-returns_df = pd.DataFrame(greater_df_list, columns=['ticker','buy_date','stochastic_value','sell_date', 'return'])
-returns_df = returns_df[returns_df.sell_date != 'Sell_Signal']
+returns_df = pd.DataFrame(greater_df_list, columns=['ticker','buy_date','stochastic_value', 'buy_price','sell_price','sell_date', 'return'])
+returns_df = returns_df[returns_df.stochastic_value <= 5]
 returns_df.to_csv('returns_summary.csv')
             
 end_time = datetime.now()
